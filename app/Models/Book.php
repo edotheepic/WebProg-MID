0bdd35588a16033e470311d8bd0e2f62dd0c5ae8
edotheepic <86707396+edotheepic@users.noreply.github.com>
2022-12-03 15:19:15 +0700

    model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    public function publisher(){
+        return $this->belongsTo(Publisher::class);
+    }
+
+    public function category(){
+        return $this->belongsToMany(Category::class);
+    }
 }
